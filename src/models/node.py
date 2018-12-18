254827f8eb97414ba1773ec23a86b9ec3b2b540e
guochen <chenguo@jieyuechina.com>
2018-12-18 17:17:12 +0800

    return msg adjustment

--- src/models/node.py
+++ src/models/node.py
@@ -19,22 +19,32 @@ class Node(db.Model):
     manager = NodeManager()
 
     def to_json(self):
+        self.refresh()
         return {
             'hostname': self.hostname or self.ip,
             'ip': self.ip,
             'status': self.manager.get_status(),
-            'free_space': self.manager.get_free_space()
+            'free_space': self.manager.get_free_space(),
+            'accessibles': self.manager.get_accesses()
         }
 
     def __str__(self):
         return str(self.to_json())
 
-    def init(self):
+    def refresh(self):
+        self.manager.set_free_space()
+        self.manager.set_accesses()
         status = NodeStatus.NA
-        if not(False in self.manager.get_accesses().values()):
-            free = self.manager.get_free_space()
-            if (MIN_FREE_SPACE < free):
-                status = NodeStatus.READY
-            elif (MIN_FREE_SPACE >= free):
-                status = NodeStatus.FULL
+        try:
+            if not(False in self.manager.get_accesses().values()):
+                free = self.manager.get_free_space()
+                if (MIN_FREE_SPACE < free):
+                    status = NodeStatus.READY
+                elif (MIN_FREE_SPACE >= free):
+                    status = NodeStatus.FULL
+        except Exception as err:
+            import traceback
+            logger.error(str(err))
+            logger.error(traceback.format_exc())
+        finally:
             self.manager.set_status(status)
