348a71598422a386cb4c79e763d6d3fe4e8e6cec
cheng <cheng@keyayun.com>
2021-03-31 07:21:35 +0000

    conf template

---
+++ src/exec_st.py
@@ -0,0 +1,23 @@
+from gcutils import read_config
+
+import syncthing
+
+CONFFILE = 'cicada.conf'
+
+cicada = read_config(CONFFILE, allow_empty=False)
+sts = []
+
+cluster = cicada.get('cluster', {})
+
+for nodename in cicada:
+    if nodename.startswith('node'):
+        node = cicada[nodename]
+        sts.append(syncthing.Syncthing(api_key=node['id'], host=node['ip']))
+
+max_replica = min(int(cluster.get('max_replica', len(sts))), len(sts))
+
+print(sts)
+print(max_replica)
+for st in sts:
+    print(st)
+    print(st.system.status())
