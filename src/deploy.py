cd1e60ba9be1c961209e87116325082aa483b7ed
gcd0318 <gcd0318@hotmail.com>
2021-07-19 15:05:42 +0800

    create path

--- src/deploy.py
+++ src/deploy.py
@@ -4,22 +4,28 @@ import sys
 from gcutils.misc import read_config
 from gcutils.cli import exec_cmd 
 
-conf = read_config('config/cicada.conf')
+conf = read_config('../config/cicada.conf')
 nodename = sys.argv[1]
 
-for sect in conf['cluster']['nodes'].split():
-    print(sect, conf[sect])
-
 node = conf[nodename]
-incoming = os.sep.join([node['root'], node['incoming']])
-backup = os.sep.join([node['root'], node['backup']])
-storage = os.sep.join([node['root'], node['storage']])
+incoming = os.path.realpath(os.path.expanduser(os.sep.join([node['root'], node['incoming']])))
+backup = os.path.realpath(os.path.expanduser(os.sep.join([node['root'], node['backup']])))
+storage = os.path.realpath(os.path.expanduser(os.sep.join([node['root'], node['storage']])))
+
+nodes = conf['cluster']['nodes'].split()
+max_replica = min(int(conf['cluster']['max_replica']), len(nodes))
+
+if not os.path.exists(incoming):
+    os.makedirs(incoming)
+for i in range(max_replica):
+    os.makedirs(os.sep.join([incoming, str(i + 1)]))
+
 for p in (backup, storage):
-    for s in conf['cluster']['nodes'].split():
+    for s in nodes:
         tgt_path = os.sep.join([p, s])
         if ((s != nodename) and (not os.path.exists(tgt_path))):
             os.makedirs(tgt_path)
-        else:
-            print(tgt_path, 'exists', os.path.exists(tgt_path))
+
+        print(tgt_path, 'exists', os.path.exists(tgt_path))
 
 
