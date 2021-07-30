a88382f67899a10ee917a996aec5de77e923e91d
cheng <cheng@keyayun.com>
2021-07-30 08:54:30 +0000

    return free_limit as digit

--- src/load_config.py
+++ src/load_config.py
@@ -1,4 +1,5 @@
 import os
+import re
 import sys
 
 from gcutils.fileops import makedirs
@@ -11,6 +12,7 @@ def load(conffile='../config/cicada.conf', nodename=None):
     conf = read_config(conffile)
     nodes = conf['cluster']['nodes'].split()
     max_replica = min(int(conf['cluster']['max_replica']), len(nodes))
+    free_limit = '100M'
 
     if nodename is not None:
         node = conf[nodename]
@@ -18,5 +20,16 @@ def load(conffile='../config/cicada.conf', nodename=None):
         incoming = os.sep.join([node['root'], node['incoming']])
         backup = os.sep.join([node['root'], node['backup']])
         storage = os.sep.join([node['root'], node['storage']])
+        free_limit = node.get('free_limit', '100M').upper()
 
-    return nodes, incoming, backup, storage, max_replica
+
+    unit_match = 'BKMGT'
+    if free_limit:
+        free_limit = '0' + free_limit
+        free_limit_unit = re.findall('\D$', free_limit)[0]
+        free_limit = int(''.join(re.findall('\d*', free_limit)))
+        for i in range(unit_match.index(free_limit_unit)):
+            free_limit = free_limit * 1024
+
+
+    return nodes, incoming, backup, storage, max_replica, free_limit
