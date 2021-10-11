bda2bded4b8b1ab7db7ff904e636f172ab7d896f
dev <gcd0318@126.com>
2021-10-11 16:19:37 +0800

    no nodename when load_config

--- src/load_config.py
+++ src/load_config.py
@@ -7,30 +7,61 @@ from gcutils.misc import read_config
 from gcutils.cli import exec_cmd 
 
 def load(conffile='../config/cicada.conf', nodename=None):
-    incoming, backup, storage = None, None, None
+    res = {}
 
     conf = read_config(conffile)
     nodes = conf['cluster']['nodes'].split()
     max_replica = min(int(conf['cluster']['max_replica']), len(nodes))
-    free_limit = '100M'
 
-    if nodename is not None:
+    if nodename is None:
+        for nname in nodes:
+            node = conf[nname]
+            tmp = {}
+            for key in conf[nname]:
+                if ('_path' in key):
+                    tmp[key] = os.sep.join([node['root'], node[key]])
+                elif ('free_limit' == key):
+                    free_limit = node.get('free_limit', '100M').upper()
+                    unit_match = 'BKMGT'
+                    free_limit = '0' + free_limit
+                    free_limit_unit = re.findall('\D$', free_limit)[0]
+                    free_limit = int(''.join(re.findall('\d*', free_limit)))
+                    for i in range(unit_match.index(free_limit_unit)):
+                        free_limit = free_limit * 1024
+                    tmp[key] = free_limit
+                else:
+                    tmp[key] = node[key]
+            res[nname] = tmp
+    else:
         node = conf[nodename]
 
         ip = node['ip']
-        incoming_path = os.sep.join([node['root'], node['incoming']])
-        backup_path = os.sep.join([node['root'], node['backup']])
-        storage_path = os.sep.join([node['root'], node['storage']])
+        incoming_path = os.sep.join([node['root'], node['incoming_path']])
+        backup_path = os.sep.join([node['root'], node['backup_path']])
+        storage_path = os.sep.join([node['root'], node['storage_path']])
+
         free_limit = node.get('free_limit', '100M').upper()
-
-
-    unit_match = 'BKMGT'
-    if free_limit:
+        unit_match = 'BKMGT'
         free_limit = '0' + free_limit
         free_limit_unit = re.findall('\D$', free_limit)[0]
         free_limit = int(''.join(re.findall('\d*', free_limit)))
         for i in range(unit_match.index(free_limit_unit)):
             free_limit = free_limit * 1024
 
+        res = {
+                'nodes': nodes,
+                'ip': ip,
+                'incoming_path': incoming_path,
+                'backup_path': backup_path,
+                'storage_path': storage_path,
+                'max_replica': max_replica,
+                'free_limit': free_limit,
+                }
 
-    return nodes, ip, incoming_path, backup_path, storage_path, max_replica, free_limit
+
+    return res
+
+if ('__main__' == __name__):
+    print(load())
+    from gcutils.netops import get_local_hostname
+    print(load(nodename=get_local_hostname()))
